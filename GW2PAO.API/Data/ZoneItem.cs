763d13747ad855172e25a637be470af765c97c19
SamHurne <SamHurne@gmail.com>
2014-08-03 14:59:57 -0400

    Fixed issue that prevented unlocked zone items from loading properly

--- GW2PAO.API/Data/ZoneItem.cs
+++ GW2PAO.API/Data/ZoneItem.cs
@@ -16,5 +16,28 @@ namespace GW2PAO.API.Data
         public string MapName { get; set; }
         public int Level { get; set; }
         public Point Location { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj != null
+                && obj is ZoneItem)
+            {
+                ZoneItem other = obj as ZoneItem;
+
+                return (other.ID == this.ID)
+                    && (other.Name == this.Name)
+                    && (other.Type == this.Type)
+                    && (other.MapId == this.MapId)
+                    && (other.MapName == this.MapName)
+                    && (other.Level == this.Level)
+                    && (other.Location.X == this.Location.X)
+                    && (other.Location.Y == this.Location.Y)
+                    && (other.Location.Z == this.Location.Z);
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
