7004e63aa5261e9e2e0fe2137c2f8357351d8ff0
SamHurne <SamHurne@gmail.com>
2014-08-31 13:35:48 -0400

    Commit of missing source files for the Teamspeak overlay.

---
+++ GW2PAO.TS3/Constants/Properties.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GW2PAO.TS3.Constants
+{
+    public static class Properties
+    {
+        public const string ServerHandlerID = "schandlerid";
+        public const string ChannelID = "cid";
+        public const string TargetChannelID = "ctid";
+        public const string ClientID = "clid";
+        public const string ClientNickname = "client_nickname";
+
+        public const string InvokerID = "invokerid";
+        public const string InvokerName = "invokername";
+        public const string Message = "msg";
+    }
+}
---
+++ GW2PAO.TS3/Data/Client.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GW2PAO.TS3.Data
+{
+    public class Client
+    {
+        public uint ID { get; private set; }
+        public string Name { get; set; }
+        public uint ChannelID { get; set; }
+
+        public Client(uint id)
+        {
+            this.ID = id;
+        }
+
+        public Client(uint id, string name, uint channelID)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.ChannelID = channelID;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj != null
+                && obj is Client)
+            {
+                Client other = obj as Client;
+
+                return other.ID == this.ID;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ID.GetHashCode();
+        }
+    }
+}
