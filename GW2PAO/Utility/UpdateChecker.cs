2383ec92501494ca1aa4d2677c44d5e3b9f6fbe0
SamHurne <SamHurne@gmail.com>
2014-09-16 20:36:15 -0400

    Added UpdateChecker utility class for checking to see if a new version of the software is available.

---
+++ GW2PAO/Utility/UpdateChecker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GW2PAO.Utility
+{
+    public static class UpdateChecker
+    {
+        /// <summary>
+        /// Synchronous check to see if a new version of the software is available
+        /// Based on http://www.hanselman.com/blog/HowToAutomaticallyNotifyTheUserThatItsTimeToUpgradeAWindowsApp.aspx
+        /// </summary>
+        /// <returns>True if a new version is available, else false</returns>
+        public static bool IsNewVersionAvailable()
+        {
+            var http = new HttpClient();
+            var request = http.GetStringAsync(new Uri("https://raw.githubusercontent.com/SamHurne/gw2pao/master/.gitignore"));
+            request.Wait(); // Should be pretty quick
+
+            string versionString = request.Result;
+            Version latestVersion = new Version(versionString);
+
+            // Get the assembly version and compare
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            Version myVersion = new Version(fvi.ProductVersion);
+
+            if (latestVersion > myVersion)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
