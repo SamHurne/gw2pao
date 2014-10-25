d73ed8f363f20064b57a38e5fec04eb7d98fb520
SamHurne <SamHurne@gmail.com>
2014-10-25 10:14:56 -0400

    Added the GuildService unit tests to source control

---
+++ UnitTests/GW2PAO.API.UnitTest/GuildServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GW2PAO.API.UnitTest
+{
+    [TestClass]
+    public class GuildServiceTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
