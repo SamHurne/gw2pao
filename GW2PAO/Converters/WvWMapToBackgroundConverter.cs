0fb91cbb88c93a1a20189f4491a3ac0621844d94
SamHurne <SamHurne@gmail.com>
2015-02-15 16:53:47 -0500

    Updated WvW notifications to have a background matching the map that they occured on

---
+++ GW2PAO/Converters/WvWMapToBackgroundConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using GW2PAO.API.Data.Enums;
+
+namespace GW2PAO.Converters
+{
+    public class WvWMapToBackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            WvWMap map = (WvWMap)value;
+
+            switch (map)
+            {
+                case WvWMap.BlueBorderlands:
+                    return "/Images/Backgrounds/Small1_b.png";
+                case WvWMap.GreenBorderlands:
+                    return "/Images/Backgrounds/Small1_g.png";
+                case WvWMap.RedBorderlands:
+                    return "/Images/Backgrounds/Small1_r.png";
+                case WvWMap.EternalBattlegrounds:
+                case WvWMap.Unknown:
+                default:
+                    return "/Images/Backgrounds/Small1.png";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
