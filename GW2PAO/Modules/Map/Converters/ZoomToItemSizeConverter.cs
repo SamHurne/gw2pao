5ea3989ea55b10a97869d1605a5ee021f8f8d3bf
shurne <samhurne@gmail.com>
2015-10-11 12:35:31 -0400

    Added a new ZoomToItemSizeConverter to support autosizing points displayed on the map as the user zoom in/out

---
+++ GW2PAO/Modules/Map/Converters/ZoomToItemSizeConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace GW2PAO.Modules.Map.Converters
+{
+    public class ZoomToItemSizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Higher zoom means larger item sizes, with a max size of 35
+            return (double)value * (35 / 7);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
