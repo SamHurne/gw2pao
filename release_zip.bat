c53940d0be7e0f0036cd31ed9f7d5865e833e178
shurne <samhurne@gmail.com>
2016-07-08 23:53:33 -0400

    Fixed incorrect slashes...

--- release_zip.bat
+++ release_zip.bat
@@ -3,14 +3,14 @@
 set VERSION=%1
 
 pushd GW2PAO\bin\x86\Release
-del /Q Logs/*
-del /Q UserData/*
+del /Q Logs\*
+del /Q UserData\*
 del /Q debug.log
 7z a ..\..\..\..\GW2PAO_%VERSION%.zip *
 popd
 pushd GW2PAO\bin\x86\Release_WithoutBrowser
-del /Q Logs/*
-del /Q UserData/*
+del /Q Logs\*
+del /Q UserData\*
 del /Q debug.log
 7z a ..\..\..\..\GW2PAO_%VERSION%_NoBrowser.zip *
 popd
