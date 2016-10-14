4c3f43bf6aee3f95704694368006678cc6c5774c
shurne <samhurne@gmail.com>
2016-10-13 20:19:30 -0400

    Updated strings for meta event stages

--- GW2PAO.API/Providers/MetaEventStageNamesProvider.cs
+++ GW2PAO.API/Providers/MetaEventStageNamesProvider.cs
@@ -126,58 +126,58 @@ namespace GW2PAO.API.Providers
                 new MetaEventStageName() { ID = MetaEventStageID.DragonsStand_MapActive,     Name = "Map Active" }
             };
 
-            // TODO: Spanish
+            // Spanish
             List<MetaEventStageName> spanish = new List<MetaEventStageName>()
             {
-                new MetaEventStageName() { ID = MetaEventStageID.DryTop_CrashSite,           Name = "Crash Site" },
-                new MetaEventStageName() { ID = MetaEventStageID.DryTop_Sandstorm,           Name = "Sandstorm" },
-                new MetaEventStageName() { ID = MetaEventStageID.VerdantBrink_NightBosses,   Name = "Night Bosses" },
-                new MetaEventStageName() { ID = MetaEventStageID.VerdantBrink_Daytime,       Name = "Daytime" },
-                new MetaEventStageName() { ID = MetaEventStageID.VerdantBrink_Night,         Name = "Night" },
-                new MetaEventStageName() { ID = MetaEventStageID.AuricBasin_Challenges,      Name = "Challenges" },
+                new MetaEventStageName() { ID = MetaEventStageID.DryTop_CrashSite,           Name = "Lugar del accidente" },
+                new MetaEventStageName() { ID = MetaEventStageID.DryTop_Sandstorm,           Name = "Tormenta de arena" },
+                new MetaEventStageName() { ID = MetaEventStageID.VerdantBrink_NightBosses,   Name = "Los jefes noche" },
+                new MetaEventStageName() { ID = MetaEventStageID.VerdantBrink_Daytime,       Name = "Tiempo de día" },
+                new MetaEventStageName() { ID = MetaEventStageID.VerdantBrink_Night,         Name = "Noche" },
+                new MetaEventStageName() { ID = MetaEventStageID.AuricBasin_Challenges,      Name = "desafíos" },
                 new MetaEventStageName() { ID = MetaEventStageID.AuricBasin_Octovine,        Name = "Octovine" },
-                new MetaEventStageName() { ID = MetaEventStageID.AuricBasin_Reset,           Name = "Reset" },
-                new MetaEventStageName() { ID = MetaEventStageID.AuricBasin_Pylons,          Name = "Pylons" },
-                new MetaEventStageName() { ID = MetaEventStageID.TangledDepths_Preparation,  Name = "Preparation" },
+                new MetaEventStageName() { ID = MetaEventStageID.AuricBasin_Reset,           Name = "Reiniciar" },
+                new MetaEventStageName() { ID = MetaEventStageID.AuricBasin_Pylons,          Name = "pilones" },
+                new MetaEventStageName() { ID = MetaEventStageID.TangledDepths_Preparation,  Name = "Preparación" },
                 new MetaEventStageName() { ID = MetaEventStageID.TangledDepths_ChakGerent,   Name = "Chak Gerent" },
-                new MetaEventStageName() { ID = MetaEventStageID.TangledDepths_HelpOutposts, Name = "Help Outposts" },
-                new MetaEventStageName() { ID = MetaEventStageID.DragonsStand_MapActive,     Name = "Map Active" }
+                new MetaEventStageName() { ID = MetaEventStageID.TangledDepths_HelpOutposts, Name = "Ayuda Outposts" },
+                new MetaEventStageName() { ID = MetaEventStageID.DragonsStand_MapActive,     Name = "mapa activo" }
             };
 
-            // TODO: French
+            // French
             List<MetaEventStageName> french = new List<MetaEventStageName>()
             {
-                new MetaEventStageName() { ID = MetaEventStageID.DryTop_CrashSite,           Name = "Crash Site" },
-                new MetaEventStageName() { ID = MetaEventStageID.DryTop_Sandstorm,           Name = "Sandstorm" },
-                new MetaEventStageName() { ID = MetaEventStageID.VerdantBrink_NightBosses,   Name = "Night Bosses" },
-                new MetaEventStageName() { ID = MetaEventStageID.VerdantBrink_Daytime,       Name = "Daytime" },
-                new MetaEventStageName() { ID = MetaEventStageID.VerdantBrink_Night,         Name = "Night" },
-                new MetaEventStageName() { ID = MetaEventStageID.AuricBasin_Challenges,      Name = "Challenges" },
-                new MetaEventStageName() { ID = MetaEventStageID.AuricBasin_Octovine,        Name = "Octovine" },
+                new MetaEventStageName() { ID = MetaEventStageID.DryTop_CrashSite,           Name = "Site du crash" },
+                new MetaEventStageName() { ID = MetaEventStageID.DryTop_Sandstorm,           Name = "Tempête de Sable" },
+                new MetaEventStageName() { ID = MetaEventStageID.VerdantBrink_NightBosses,   Name = "Boss de nuit" },
+                new MetaEventStageName() { ID = MetaEventStageID.VerdantBrink_Daytime,       Name = "Jour" },
+                new MetaEventStageName() { ID = MetaEventStageID.VerdantBrink_Night,         Name = "Nuit" },
+                new MetaEventStageName() { ID = MetaEventStageID.AuricBasin_Challenges,      Name = "Défis" },
+                new MetaEventStageName() { ID = MetaEventStageID.AuricBasin_Octovine,        Name = "Octoliane" },
                 new MetaEventStageName() { ID = MetaEventStageID.AuricBasin_Reset,           Name = "Reset" },
-                new MetaEventStageName() { ID = MetaEventStageID.AuricBasin_Pylons,          Name = "Pylons" },
-                new MetaEventStageName() { ID = MetaEventStageID.TangledDepths_Preparation,  Name = "Preparation" },
-                new MetaEventStageName() { ID = MetaEventStageID.TangledDepths_ChakGerent,   Name = "Chak Gerent" },
-                new MetaEventStageName() { ID = MetaEventStageID.TangledDepths_HelpOutposts, Name = "Help Outposts" },
-                new MetaEventStageName() { ID = MetaEventStageID.DragonsStand_MapActive,     Name = "Map Active" }
+                new MetaEventStageName() { ID = MetaEventStageID.AuricBasin_Pylons,          Name = "Pylones" },
+                new MetaEventStageName() { ID = MetaEventStageID.TangledDepths_Preparation,  Name = "Préparation" },
+                new MetaEventStageName() { ID = MetaEventStageID.TangledDepths_ChakGerent,   Name = "Régent Chak" },
+                new MetaEventStageName() { ID = MetaEventStageID.TangledDepths_HelpOutposts, Name = "Aider les campements" },
+                new MetaEventStageName() { ID = MetaEventStageID.DragonsStand_MapActive,     Name = "Carte active" }
             };
 
-            // TODO: German
+            // German
             List<MetaEventStageName> german = new List<MetaEventStageName>()
             {
                 new MetaEventStageName() { ID = MetaEventStageID.DryTop_CrashSite,           Name = "Crash Site" },
-                new MetaEventStageName() { ID = MetaEventStageID.DryTop_Sandstorm,           Name = "Sandstorm" },
-                new MetaEventStageName() { ID = MetaEventStageID.VerdantBrink_NightBosses,   Name = "Night Bosses" },
-                new MetaEventStageName() { ID = MetaEventStageID.VerdantBrink_Daytime,       Name = "Daytime" },
-                new MetaEventStageName() { ID = MetaEventStageID.VerdantBrink_Night,         Name = "Night" },
-                new MetaEventStageName() { ID = MetaEventStageID.AuricBasin_Challenges,      Name = "Challenges" },
+                new MetaEventStageName() { ID = MetaEventStageID.DryTop_Sandstorm,           Name = "Sandsturm" },
+                new MetaEventStageName() { ID = MetaEventStageID.VerdantBrink_NightBosses,   Name = "Nacht Bosses" },
+                new MetaEventStageName() { ID = MetaEventStageID.VerdantBrink_Daytime,       Name = "Tageszeit" },
+                new MetaEventStageName() { ID = MetaEventStageID.VerdantBrink_Night,         Name = "Nacht" },
+                new MetaEventStageName() { ID = MetaEventStageID.AuricBasin_Challenges,      Name = "Herausforderungen" },
                 new MetaEventStageName() { ID = MetaEventStageID.AuricBasin_Octovine,        Name = "Octovine" },
-                new MetaEventStageName() { ID = MetaEventStageID.AuricBasin_Reset,           Name = "Reset" },
-                new MetaEventStageName() { ID = MetaEventStageID.AuricBasin_Pylons,          Name = "Pylons" },
-                new MetaEventStageName() { ID = MetaEventStageID.TangledDepths_Preparation,  Name = "Preparation" },
+                new MetaEventStageName() { ID = MetaEventStageID.AuricBasin_Reset,           Name = "zurückstellen" },
+                new MetaEventStageName() { ID = MetaEventStageID.AuricBasin_Pylons,          Name = "pylons" },
+                new MetaEventStageName() { ID = MetaEventStageID.TangledDepths_Preparation,  Name = "Vorbereitung" },
                 new MetaEventStageName() { ID = MetaEventStageID.TangledDepths_ChakGerent,   Name = "Chak Gerent" },
-                new MetaEventStageName() { ID = MetaEventStageID.TangledDepths_HelpOutposts, Name = "Help Outposts" },
-                new MetaEventStageName() { ID = MetaEventStageID.DragonsStand_MapActive,     Name = "Map Active" }
+                new MetaEventStageName() { ID = MetaEventStageID.TangledDepths_HelpOutposts, Name = "Hilfe Outposts" },
+                new MetaEventStageName() { ID = MetaEventStageID.DragonsStand_MapActive,     Name = "Karte Aktiv" }
             };
 
             Serialization.SerializeToXml(english, this.GetFilePath("en"));
